c653bb0d0c9e1017a5783c370267c2f95fc2f00f
mik-305 <mik-305@bk.ru>
2024-09-03 16:21:33 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,18 @@
+class House():
+    def __init__(self, name, namber_of_floors):
+        self.name = name
+        self.namber_of_floors = namber_of_floors
+    def __len__(self):
+        return self.namber_of_floors
+    def __str__(self):
+        return 'Название '+self.name+', количество этажей '+str(self.namber_of_floors)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+# __str__
+print(h1)
+print(h2)
+# __len__
+print(len(h1))
+print(len(h2))
